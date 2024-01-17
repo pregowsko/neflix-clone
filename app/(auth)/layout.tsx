969e638ce523264deec9aafb7204f805a64da58f
Aleksandra Stanek <ola.pregowska975@gmail.com>
2024-01-17 19:36:51 +0100

    added prisma, api, utils

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -7,7 +7,7 @@ export default function AuthLayout ({children}: {children: ReactNode}) {
     return (
         <div className="relative flex h-screen w-screen flex-col bg-black md:items-center md:justify-center md:bg-transparent">
             <Image src={BackgroundImage} alt="background image" className='hidden sm:flex sm:object-cover -z-10 brightness-50' priority fill/>
-            <Image src={Logo} alt="logo" className='absolute left-4 top-4 object-contain md:left-10 md:top-6' width={120} height={120}/>
+            <Image src={Logo} alt="logo" className='absolute left-4 top-4 object-contain md:left-10 md:top-6' width={120} height={120} priority/>
             {children}
         </div>
     );
