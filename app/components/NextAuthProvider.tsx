8a142c1730daad6b93bbeaff9b270a1044c7a8f3
Aleksandra Stanek <ola.pregowska975@gmail.com>
2024-01-22 12:31:08 +0100

    added nextprovider

---
+++ app/components/NextAuthProvider.tsx
@@ -0,0 +1,6 @@
+import { SessionProvider } from "next-auth/react";
+import { ReactNode } from "react";
+
+export const NextAuthProvider = ({children}: {children: ReactNode}) => {
+    return <SessionProvider>{children}</SessionProvider>
+}
